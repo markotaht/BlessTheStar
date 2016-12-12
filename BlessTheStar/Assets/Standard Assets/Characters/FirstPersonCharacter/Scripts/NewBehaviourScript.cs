5ac79bbe57dab9aececdc856527e3dab5df43ef7
marko Täht <markotaht@gmail.com>
2016-12-12 10:18:53 +0200

    End game trigger

--- BlessTheStar/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/NewBehaviourScript.cs
+++ BlessTheStar/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/NewBehaviourScript.cs
@@ -2,19 +2,28 @@
 using System.Collections;
 namespace UnityStandardAssets.Characters.FirstPerson
 {
-public class NewBehaviourScript : MonoBehaviour {
+	public class NewBehaviourScript : MonoBehaviour {
 
-	public bool win;
-	void OnTriggerEnter(Collider other){
-		if (other.tag == "Player") {
-			GameObject ctrl = other.gameObject;
-			if (win) {
-				PlayerPrefs.SetInt ("highscore", ctrl.GetComponent<RigidbodyFirstPersonController>().getScore());
-					PlayerPrefs.SetFloat ("besttime", ctrl.GetComponent<RigidbodyFirstPersonController>().getTime ());
+		public bool win;
+		void OnTriggerEnter(Collider other){
+			if (other.tag == "Player") {
+				GameObject ctrl = other.gameObject;
+				int old_score = ctrl.GetComponent<RigidbodyFirstPersonController>().getScore();
+				float old_time=  ctrl.GetComponent<RigidbodyFirstPersonController>().getTime ();
+				if (PlayerPrefs.GetInt ("highscore") < old_score) {
+					PlayerPrefs.GetInt ("highscore", old_score);
+				}
+
+				if (win) {
+					if (PlayerPrefs.GetInt ("highscore") < old_score) {
+						PlayerPrefs.GetFloat ("besttime", old_time);
+					}
+				} else {
+
+				}
+				Application.LoadLevel (0);
+				Cursor.lockState = CursorLockMode.None;
 			}
-			Application.LoadLevel (0);
-			Cursor.lockState = CursorLockMode.None;
 		}
 	}
-}
 }
